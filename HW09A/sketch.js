5548f10fa60f3c3803e5888377fcaf0ada7878bd
xingyu yao <xy2779@nyu.edu>
2024-11-09 23:38:59 -0500

    hw09

--- HW09A/sketch.js
+++ HW09A/sketch.js
@@ -1,10 +1,10 @@
-
 // original image, to use as reference for pixel colors
 let oImg;
 
 // display image, to modify and display on canvas
 let mImg;
-
+let slider;
+let texture = [];
 function preload() {
   oImg = loadImage("../assets/mondriaan.jpg");
   mImg = loadImage("../assets/mondriaan.jpg");
@@ -13,25 +13,49 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   oImg.resize(0, height);
-  mImg.resize(0, height);
+  mImg.resize(0, height); // we'll read pixel color info from the oImg, so let's load its pixels
 
-  // we'll read pixel color info from the oImg, so let's load its pixels
-  oImg.loadPixels();
+  oImg.loadPixels(); // TODO: setup sliders and other DOM/html elements here
 
-  // TODO: setup sliders and other DOM/html elements here
+  slider = createSlider(0, 255);
+  slider.position((height * 5) / 4, height / 2);
+  slider.size(320);
 }
 
 function draw() {
   // we'll modify and display the mImg object, so let's load its pixels
+  let s = slider.value();
   mImg.loadPixels();
+  for (let idx = 0; idx < mImg.pixels.length; idx += 4) {
+    let r = mImg.pixels[idx + 0];
+    let g = mImg.pixels[idx + 1];
+    let b = mImg.pixels[idx + 2];
+    let pixelIsR = r > 1.3 * g && r > 1.3 * b && r > 128;
+    let pixelIsG = g > 1.8 * r && g > 0.4 * b && g > 50;
+    let pixelIsY = r > 1.2 * b && g > 1.2 * b && r > 128 && g > 128;
+    if (pixelIsR) {
+      oImg.pixels[idx + 0] = 89;
+      oImg.pixels[idx + 1] = 55 + s / 2;
+      oImg.pixels[idx + 2] = 191;
+    }
+    if (pixelIsG) {
+      oImg.pixels[idx + 0] = 255;
+      oImg.pixels[idx + 1] = 178;
+      oImg.pixels[idx + 2] = 77 + s;
+    }
+    if (pixelIsY) {
+      oImg.pixels[idx + 0] = 222;
+      oImg.pixels[idx + 1] = 0 + s;
+      oImg.pixels[idx + 2] = 0;
+    }
+  }
+  oImg.updatePixels(); // draw the display image
 
-  // TODO: do any filtering and pixel modifications here.
-  //       This involves a for loop of some kind.
-  //       Remember to read from the oImg pixels and write to the mImg.
-
-  // we'll display the updated mImg, so let's update its pixels
-  mImg.updatePixels();
-
-  // draw the display image
-  image(mImg, 0, 0);
+  image(oImg, 0, 0);
 }
+
+// TODO: do any filtering and pixel modifications here.
+//       This involves a for loop of some kind.
+//       Remember to read from the oImg pixels and write to the mImg.
+
+// we'll display the updated mImg, so let's update its pixels
--- HW09B/sketch.js
+++ HW09B/sketch.js
@@ -1,16 +1,63 @@
 let mCamera;
+let blockSize = 20; // 基础方块大小
 
 function preload() {
   mCamera = createCapture(VIDEO);
-  mCamera.size();
+  mCamera.size(windowWidth, windowHeight);
   mCamera.hide();
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
-  background(180, 200, 255);
-  image(mCamera, 0, 0);
+  background(0);
+
+  // 加载摄像头像素数据
+  mCamera.loadPixels();
+
+  // 遍历每个像素块，增加间距
+  let spacing = blockSize * 1.5; // 增加间距
+  for (let y = 0; y < height; y += spacing) {
+    for (let x = 0; x < width; x += spacing) {
+
+      // 为每个方块位置随机添加偏移，制造失真效果
+      let offsetX = x + random(-10, 10);
+      let offsetY = y + random(-10, 10);
+
+      // 获取当前像素块的颜色
+      let i = (floor(offsetY) * mCamera.width + floor(offsetX)) * 4;
+      let r = mCamera.pixels[i];
+      let g = mCamera.pixels[i + 1];
+      let b = mCamera.pixels[i + 2];
+
+      // 计算当前像素块的亮度
+      let brightness = (r + g + b) / 3;
+
+      // 根据亮度设置方块大小，亮度高则方块大，亮度低则方块小
+      let rectSize = map(brightness, 0, 255, blockSize * 0.3, blockSize * 1.8);
+
+      // 计算重叠矩形的数量（亮度低时重叠更多）
+      let overlapCount = int(map(brightness, 0, 255, 5, 1));
+
+      // 设置粉色轮廓，无填充
+      stroke(255, 105, 180);
+      noFill();
+
+      // 绘制多个重叠的小矩形
+      for (let j = 0; j < overlapCount; j++) {
+        // 设置随机的描边粗细
+        strokeWeight(random(0.5, 2.5));
+        
+        // 随机微调位置，产生重叠效果
+        let offsetRectX = x + random(-5, 5);
+        let offsetRectY = y + random(-5, 5);
+
+        // 绘制大小随亮度变化的粉色矩形轮廓
+        rect(offsetRectX, offsetRectY, rectSize, rectSize);
+      }
+    }
+  }
 }
